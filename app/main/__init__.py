1b95b4a3e77312acf9b35c6dcb3244c3fb1aade6
akosfi <fiakos1997@gmail.com>
2019-11-12 11:22:48 +0100

    send static files

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -12,6 +12,8 @@ flask_bcrypt = Bcrypt()
 def create_app(config_name):
     app = Flask(__name__, static_url_path='')
     app.config.from_object(config_by_name[config_name])
+    app.config['UPLOAD_FOLDER'] = 'static/img'
+
     db.init_app(app)
     flask_bcrypt.init_app(app)
 
--- app/main/controller/message_controller.py
+++ app/main/controller/message_controller.py
@@ -1,18 +1,34 @@
 import jwt
+import os
+import uuid
 
 from flask import request, after_this_request
 from flask_restplus import Resource
-
+from werkzeug.utils import secure_filename
 
 from app.main.model.message import Message
 
-from ..util import jwt_required
+from ..util import jwt_required, create_response_object
 from ..util.dto import MessageDto
-from ..config import key
+from ..config import key, basedir
 from ..service.message_service import get_messages_of_user
 
 api = MessageDto.api
 _message = MessageDto.message
+
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_unique_filename(filename):
+    
+    generated = str(uuid.uuid4()) 
+    extension = filename.rsplit('.', 1)[1].lower()
+
+    return '.'.join([generated, extension]) 
 
 
 @api.route('/')
@@ -26,4 +42,23 @@ class MessageList(Resource):
         return get_messages_of_user(payload['user']['id'])
 
 
-    
+@api.route('/image')
+class MessageImage(Resource): 
+
+    @jwt_required
+    def post(self):
+        if 'file' not in request.files:
+            return create_response_object(404, 'File not found.'), 404
+        file = request.files['file']
+        if file.filename == '':
+            return create_response_object(404, 'File not found.'), 404
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            unique_filename = get_unique_filename(filename)
+            file.save(os.path.join(basedir, 'static/img', unique_filename))
+            return create_response_object(200, 'File uploaded succesfully.', unique_filename), 200
+
+
+
+
+
