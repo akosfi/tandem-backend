46fe11520a680f734d5b03512b80c1ca6154395d
akosfi <fiakos1997@gmail.com>
2019-11-14 21:40:56 +0100

    try heroku

--- manage.py
+++ manage.py
@@ -29,7 +29,7 @@ socketio = create_socket_app(app)
 
 @manager.command
 def run():
-    socketio.run(app, host="tandem-bme.herokuapp.com", port="80")
+    socketio.run(app)
 
 @manager.command
 def seed():
