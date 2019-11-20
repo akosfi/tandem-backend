839f8425e8b4fa6fa8b0fb6226dd2dc369a97af0
akosfi <fiakos1997@gmail.com>
2019-11-20 10:37:49 +0100

    added built react app

---
+++ app/main/public/precache-manifest.372500c4fb599a81844e7b3502447145.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0f746ca950852509968e677c24aa60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30f266b50f937aaa7e67",
+    "url": "/static/css/2.b29c7886.chunk.css"
+  },
+  {
+    "revision": "1267aeb7cdf4a6b03c0d",
+    "url": "/static/css/main.29065af1.chunk.css"
+  },
+  {
+    "revision": "30f266b50f937aaa7e67",
+    "url": "/static/js/2.42f125ee.chunk.js"
+  },
+  {
+    "revision": "1267aeb7cdf4a6b03c0d",
+    "url": "/static/js/main.3a8f329f.chunk.js"
+  },
+  {
+    "revision": "89e365c754309ef5b060",
+    "url": "/static/js/runtime-main.80bbae50.js"
+  },
+  {
+    "revision": "782154b83b6b419269980b375144b1f7",
+    "url": "/static/media/icons-16.782154b8.ttf"
+  },
+  {
+    "revision": "7fb844a70d4ff3aa49441242b4149777",
+    "url": "/static/media/icons-16.7fb844a7.woff"
+  },
+  {
+    "revision": "d4f6722b1ac527dd937155d419ec8ddd",
+    "url": "/static/media/icons-16.d4f6722b.eot"
+  },
+  {
+    "revision": "90afdd237a469c44c9a1a3b0ff2db197",
+    "url": "/static/media/icons-20.90afdd23.eot"
+  },
+  {
+    "revision": "c0e4874e23b8c9100e566ca2ca287128",
+    "url": "/static/media/icons-20.c0e4874e.woff"
+  },
+  {
+    "revision": "d1f0f6c1a1c239a03914f7bd06ccfe68",
+    "url": "/static/media/icons-20.d1f0f6c1.ttf"
+  }
+]);
